7306633b53b95f95acb30a8ea7d6d9872b9bec5e
Dhanavarshini <dp9387@srmist.edu.in>
2026-05-05 12:05:50 +0530

    [dhana]:temp commit before move

--- order-service/src/main/java/com/bridgelabz/order_service/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bridgelabz.order_service.repository;
-
-import com.bridgelabz.order_service.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
-}
