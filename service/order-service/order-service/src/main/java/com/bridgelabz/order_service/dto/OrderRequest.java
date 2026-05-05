f571e11d2acb022afdf8c0f0974c3b803f2de748
Dhanavarshini <dp9387@srmist.edu.in>
2026-05-05 12:46:03 +0530

    [dhana]: removed duplicate nested order-service folder

--- service/order-service/order-service/src/main/java/com/bridgelabz/order_service/dto/OrderRequest.java
+++
@@ -1,9 +0,0 @@
-package com.bridgelabz.order_service.dto;
-import lombok.Data;
-
-@Data
-public class OrderRequest {
-    private String userEmail;
-    private Long productId;
-    private int quantity;
-}
